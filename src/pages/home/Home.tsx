e15299352cd0d2f4218af48569db25de7228f840
Emmanuelsteph7 <stephenemmanuel770@gmail.com>
2022-04-29 08:54:17 +0100

    test

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import { jumboData } from "data/jumboData";
 import JumboSection from "./components/JumboSection";
 
-const Home = () => {
-  const varia = "dd";
-  return (
-    <>
-      {jumboData.map((item) => (
-        <JumboSection key={item.id} item={item} />
-      ))}
-    </>
-  );
-};
+const Home = () => (
+  <>
+    {jumboData.map((item) => (
+      <JumboSection key={item.id} item={item} />
+    ))}
+  </>
+);
 
 export default Home;
